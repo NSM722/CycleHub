f328c63978595f717df1c6a9998057285cfed8db
Natalie <nataliembatha@gmail.com>
2023-02-22 01:20:04 +0100

    Code refactoring

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-      <footer className="bg-dark p-3 container-fluid text-center text-secondary fs-5">
-          <p>Follow us for updates @myGithubPage</p>
-          <hr />
-          <p className="mt-2">Copyright &copy; BikEEE LLC 2022</p>
-          <a className="text-muted" 
-          href="http://www.termify.io" 
-          target="_blank" 
+      <footer className="bg-dark p-3 container-fluid text-center text-white fs-5">
+        <p>Follow us for updates @myGithubPage</p>
+        <hr />
+        <p className="mt-2">Copyright &copy; BikEEE LLC 2022</p>
+        <a className="text-warning"
+          href="http://www.termify.io"
+          target="_blank"
           rel="noreferrer"
-          id="terms"> 
+          id="terms">
           Terms & conditions apply</a>
       </footer>
     </>
--- src/components/PopularModels/PopularModels.js
+++ src/components/PopularModels/PopularModels.js
@@ -7,24 +7,24 @@ const PopularModels = () => {
       <section>
         <div className="container-fluid text-center mt-5">
           <div className="row">
-              <div className="col p-2">
-                  <h3 className='fw-bold mb-4'>Heading 1</h3>
-                  <img src="https://picsum.photos/id/1077/300" className="rounded-circle border border-dark mb-3" alt="highway"/>  
-                  <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>  
+            <div className="col p-2">
+              <h3 className='fw-bold mb-4'>Heading 1</h3>
+              <img src="https://picsum.photos/id/1077/300" className="rounded-circle border border-dark mb-3" alt="cyclists on the road" />
+              <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>
 
-              </div>
-              <div className="col p-2">
-                  <h3 className='fw-bold mb-4'>Heading 2</h3>
-                  <img src="https://picsum.photos/id/146/300" className="rounded-circle border border-dark mb-3" alt="lamps" /> 
-                  <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>  
-  
-              </div>
-              <div className="col p-2">
-                  <h3 className='fw-bold mb-4'>Heading 3</h3>
-                  <img src="https://picsum.photos/id/212/300" className="rounded-circle border border-dark mb-3" alt="pedestrian" />  
-                  <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>  
+            </div>
+            <div className="col p-2">
+              <h3 className='fw-bold mb-4'>Heading 2</h3>
+              <img src="https://picsum.photos/id/146/300" className="rounded-circle border border-dark mb-3" alt="tiny red bicycle chained to a pole" />
+              <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>
 
-              </div>
+            </div>
+            <div className="col p-2">
+              <h3 className='fw-bold mb-4'>Heading 3</h3>
+              <img src="https://picsum.photos/id/212/300" className="rounded-circle border border-dark mb-3" alt="parking space for bikes on a street" />
+              <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>
+
+            </div>
           </div>
         </div>
       </section>
