d11d5ae0b581c8d8491972bb062b5941d03a4d18
Natalie <nataliembatha@gmail.com>
2023-03-09 17:44:27 +0100

    OP-BGJISJS MINGW64 /f/Web_Projects/BikEEE/my-app (main)

---
+++ src/popularModelsData.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-expressions, import/no-anonymous-default-export
+export default [
+  {
+    id: 1,
+    heading: 'Heading 1',
+    src: 'https://picsum.photos/id/1077/300'
+  },
+  {
+    id: 2,
+    heading: 'Heading 2',
+    src: 'https://picsum.photos/id/146/300'
+  },
+  {
+    id: 3,
+    heading: 'Heading 3',
+    src: 'https://picsum.photos/id/212/300'
+  }
+]
