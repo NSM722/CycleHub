5270ad5537060f70e840f1444e9b6e6170d347aa
Natalie <nataliembatha@gmail.com>
2023-03-09 17:45:39 +0100

    Refined the JSX elements by mapping through data

--- src/components/PopularModels/PopularModels.js
+++ src/components/PopularModels/PopularModels.js
@@ -1,28 +1,23 @@
 // This component renders 3 random pics and some lorem picsum text block
 import React from 'react';
+import popularModelsData from '../../popularModelsData';
 
 const PopularModels = () => {
+  const popularModels = popularModelsData.map(({ id, heading, src }) => (
+    <div className="col p-2" key={id}>
+      <h3 className='fw-bold mb-4'>{heading}</h3>
+      <img
+        src={src}
+        className="rounded-circle border border-dark mb-3"
+        alt="cyclists on the road" />
+      <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>
+    </div>
+
+  ))
   return (
     <section className="container-fluid text-center mt-5">
       <div className="row">
-        <div className="col p-2">
-          <h3 className='fw-bold mb-4'>Heading 1</h3>
-          <img src="https://picsum.photos/id/1077/300" className="rounded-circle border border-dark mb-3" alt="cyclists on the road" />
-          <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>
-
-        </div>
-        <div className="col p-2">
-          <h3 className='fw-bold mb-4'>Heading 2</h3>
-          <img src="https://picsum.photos/id/146/300" className="rounded-circle border border-dark mb-3" alt="tiny red bicycle chained to a pole" />
-          <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>
-
-        </div>
-        <div className="col p-2">
-          <h3 className='fw-bold mb-4'>Heading 3</h3>
-          <img src="https://picsum.photos/id/212/300" className="rounded-circle border border-dark mb-3" alt="parking space for bikes on a street" />
-          <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis ducimus aperiam! Voluptatem expedita tempore cumque saepe, nulla porro inventore praesentium sapiente aut illo voluptas quia nemo at ipsam soluta quidem ullam iure in odio explicabo!</p>
-
-        </div>
+        {popularModels}
       </div>
     </section>
   );
